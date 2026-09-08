3d1129e1557cc3bea39d8e400a8e60f40e8555d8
David Gonzalez <dgoncor3005@gmail.com>
2026-09-08 11:12:40 +0200

    fix(portfolio): usar Vercel como URL principal

--- astro-v2/astro.config.mjs
+++ astro-v2/astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://davidgoncor3005.github.io'
+  site: 'https://davidgonzalezcordoba-dev.vercel.app'
 });
